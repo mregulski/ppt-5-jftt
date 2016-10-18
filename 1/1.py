cb6f836e3cecab84fa3517c10624659889ca9139
Marcin Regulski <mregulski@users.noreply.github.com>
2016-10-18 07:25:49 +0200

    [Lista 1] final version
    * added vscode launch config

--- 1/1.py
+++ 1/1.py
@@ -1,43 +1,49 @@
 #!/usr/bin/python
 import argparse
 import string
-
-class Matcher:
-    def __init__(self, pattern):
-        self.pattern = pattern
-        self.alphabet = string.ascii_letters
-        self.transition = {}
-
-    def compute_transitions(self):
-        m = len(self.pattern)
-        for state in range(0, m+1):
-            for symbol in self.alphabet:
-                k = min(m + 1, q + 2)
-                print("state: {}, k: {}, m: {}".format(state, k, m))
-                transition[symbol][q]
-
-
-    def search(self, text):
-        n = len(text)
-        q = 0
-        for i in range(1, n+1):
-            q = self.transition[text[i]][q]
-            if (q == m):
-                shift = i - m
-                return shift
-        return -1
+        
+alphabet = string.printable
 
 def main():
+    # print(alphabet)
     args = get_cli_args()
     pattern = get_pattern()
     text = get_text()
-    shift = Matcher(pattern).search(text)
-    if (shift != -1):
-        print("Pattern found at shift = {}".format(shift))
+    for char in pattern:
+        if char not in alphabet
+    find(pattern, text)
+    # if (shift != -1):
+    #     print("Pattern found at shift = {}".format(shift))
 
 
 def find(pattern, text):
-    print("looking for pattern '{}'".format(pattern))
+    transitions = compute_transitions(pattern)
+    current_state = 0
+    for i in range(len(text)):
+        # print("current state: {}\ni: {}\ntext[i]: {}\n".format(current_state, i,text[i]))
+        current_state = transitions[current_state][text[i]]
+        if (current_state == len(pattern)):
+            shift = i - len(pattern) + 1
+            print("found at position = {}".format(shift))
+        
+    return -1
+
+
+def compute_transitions(pattern):
+    global alphabet
+    transitions = [{} for state in range(len(pattern) + 1)]
+    for current_state in range(len(pattern) + 1):
+        for symbol in alphabet:
+            next_state = min(len(pattern) + 1, current_state + 2) - 1
+            while (not suffix(pattern[:next_state], pattern[:current_state] + symbol)):
+                next_state -= 1
+            transitions[current_state][symbol] = next_state
+    return transitions
+
+# Check if part is a suffix of the whole
+def suffix(part, whole):
+    result = whole[::-1][:len(part)] == part[::-1]
+    return result
 
 
 def get_pattern():
